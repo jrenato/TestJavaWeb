3b9e060a155af5dd689dbeb4b20e6ad58c0eccf0
Jose Renato de Carvalho <renato@lavabit.com>
2010-05-01 18:14:38 -0300

    second commit

---
+++ src/main/java/br/com/teste/TesteBean.java
@@ -0,0 +1,13 @@
+package br.com.teste;
+
+public class TesteBean {
+	private String nome = "aaa";
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
